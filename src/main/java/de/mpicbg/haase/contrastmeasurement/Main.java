802f78a844782e3f9508e691b9745e9ddd4af2c9
haesleinhuepf <rhaase@mpi-cbg.de>
2017-11-04 10:18:16 +0100

    added ArgMax projection

--- src/main/java/de/mpicbg/haase/contrastmeasurement/Main.java
+++ src/main/java/de/mpicbg/haase/contrastmeasurement/Main.java
@@ -1,7 +1,8 @@
 package de.mpicbg.haase.contrastmeasurement;
 
+import com.sun.org.apache.xpath.internal.Arg;
+import de.mpicbg.haase.contrastmeasurement.scijava.ArgMaxProjection;
 import de.mpicbg.haase.contrastmeasurement.scijava.StandardDeviationPerPixelMeasurement;
-import de.mpicbg.haase.contrastmeasurement.scijava.StandardDeviationPerSliceMeasurement;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imglib2.RandomAccessibleInterval;
@@ -14,38 +15,65 @@ public class Main
 {
   final static ImageJ ij = new ImageJ();
 
+  final static double xyDownScalingFactor = 0.25;
+
   public static void main(final String... args) throws Exception
   {
     ij.ui().showUI();
 
-    Object object = ij.io().open("C:\\structure\\data\\xwing\\2017-11-01-EDF\\EDF5_focus_going_through_lightsheet_10.tif");
-    Dataset dataset = (Dataset)object;
+    //    Object object = ij.io().open("C:\\structure\\data\\xwing\\2017-11-01-EDF\\EDF5_focus_going_through_lightsheet_10.tif");
+    //    Dataset dataset = (Dataset)object;
+    //    ij.ui().show(dataset);
+    //
+    //    System.out.println(dataset.getClass().getCanonicalName());
+    //
+    //    testStdDevCalculation(dataset);
+
+    Object
+        object =
+        ij.io()
+          .open(
+              "C:\\structure\\data\\xwing\\2017-11-01-EDF\\EDF5_focus_going_through_lightsheet_10_stdDev.tif");
+    Dataset dataset = (Dataset) object;
     ij.ui().show(dataset);
 
-    System.out.println(dataset.getClass().getCanonicalName());
+    ArgMaxProjection maxProjector = new ArgMaxProjection(dataset);
+    ij.ui().show("max", maxProjector.getMaxProjection());
+    ij.ui().show("argMax", maxProjector.getArgMaxProjection());
 
-    testStdDevCalculation(dataset);
   }
 
-  private static Img<FloatType> testStdDevCalculation(Img img) throws Exception
+  private static Img<FloatType> testStdDevCalculation(Img img) throws
+                                                               Exception
   {
 
     ij.ui().show(img);
 
-    InterpolatorFactory interpolatorFactory = new NLinearInterpolatorFactory<>();
+    InterpolatorFactory
+        interpolatorFactory =
+        new NLinearInterpolatorFactory<>();
 
-    RandomAccessibleInterval downScaledImg = ij.op().transform().scale(img, new double[] { 0.25, 0.25, 1}, interpolatorFactory);
+    RandomAccessibleInterval
+        downScaledImg =
+        ij.op()
+          .transform()
+          .scale(img,
+                 new double[] { xyDownScalingFactor, xyDownScalingFactor, 1 },
+                 interpolatorFactory);
 
     int radius = 5;
 
-//    Object[]
-//        imglibParameters =
-//        new Object[] { "radius", radius,
-//                       "image", downScaledImg};
-//
-//    ij.command().run(StandardDeviationPerSliceMeasurement.class, true, imglibParameters);
+    //    Object[]
+    //        imglibParameters =
+    //        new Object[] { "radius", radius,
+    //                       "image", downScaledImg};
+    //
+    //    ij.command().run(StandardDeviationPerSliceMeasurement.class, true, imglibParameters);
 
-    Img<FloatType> stdDevImage = StandardDeviationPerPixelMeasurement.process(downScaledImg, radius);
+    Img<FloatType>
+        stdDevImage =
+        StandardDeviationPerPixelMeasurement.process(downScaledImg,
+                                                     radius);
 
     System.out.println("Bye!");
 
